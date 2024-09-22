6c0664d7efc3b2035c3eed4c535470154fbed507
Manoj-N00 <manojnaikj@gmail.com>
2024-09-22 22:08:34 +0530

    init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,66 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
+import { sign ,verify} from 'hono/jwt';
 
-const app = new Hono()
 
-app.post('/api/v1/signup', (c) => {
-  return c.text('Hello Hono!')
-})
-app.post('/api/v1/signin', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
+
+const prisma = new PrismaClient({
+  datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate());
+
+app.post('/api/v1/signup', async (c) => {
+  const body = await c.req.json();
+  const hashedPassword = await bcrypt.hash(body.password, 10);
+
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: hashedPassword,
+    },
+  });
+
+  const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+  return c.json({ jwt: token });
+});
+
+app.post('/api/v1/signin', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where:{
+      email: body.email,
+      password:body.password,
+    }
+  });
+
+  if (!user || !(await bcrypt.compare(body.password, user.password))) {
+    c.status(403);
+    return c.json({ error: 'User not found or password incorrect' });
+  }
+
+  const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+  return c.json({ jwt: token });
+});
+
 app.post('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-app.put('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-app.get('/api/v1/blog/:id', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('Hello Hono!');
+});
 
-export default app
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!');
+});
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Hello Hono!');
+});
+
+export default app;
